c0c719e3c0234ce00e77427a8b511a8d048f8dab
Wenting Li <wenting@Wentings-MacBook-Air.local>
2018-04-19 23:05:47 -0400

    organized

--- Deque.java
+++ Deque.java
@@ -1,31 +1,30 @@
-public interface Deque<T>{
+public interface Deque<T>
+{
+    //Inserts the specified element at the front of this deque unless it 
+    //would violate capacity restrictions.
+    public boolean	offerFirst(E e);
 
-  //Inserts the specified element into the queue represented by this deque 
-  //(in other words, at the tail of this deque) if it is possible to do so 
-  // immediately without violating capacity restrictions, returning true upon 
-  // success and throwing an IllegalStateException if no space is currently 
-  // available.
+    //Inserts the specified element at the end of this deque unless it 
+    //would violate capacity restrictions.
+    public boolean	offerLast(E e);
 
-  
-    public boolean	offerFirst(E e)
-//Inserts the specified element at the front of this deque unless it would violate capacity restrictions.
+    //Retrieves, but does not remove, the first element of this deque, 
+    //or returns null if this deque is empty.
+    public E peekFirst();
 
-    public boolean	offerLast(E e)
-//Inserts the specified element at the end of this deque unless it would violate capacity restrictions.
+    //Retrieves, but does not remove, the last element of this deque, 
+    //or returns null if this deque is empty.
+    public E peekLast();
 
-    public E	peekFirst()
-//Retrieves, but does not remove, the first element of this deque, or returns null if this deque is empty.
+    //Retrieves and removes the first element of this deque, or 
+    //returns null if this deque is empty.
+    public E pollFirst();
 
-    public E	peekLast()
-//Retrieves, but does not remove, the last element of this deque, or returns null if this deque is empty.
+    //Retrieves and removes the last element of this deque, or 
+    //returns null if this deque is empty.
+    public E pollLast();
 
-    public E	pollFirst()
-//Retrieves and removes the first element of this deque, or returns null if this deque is empty.
-
-    public E	pollLast()
-//Retrieves and removes the last element of this deque, or returns null if this deque is empty.
-
-    public int	size()
-//Returns the number of elements in this deque.
+    //Returns the number of elements in this deque.
+    public int size();
 
 }
