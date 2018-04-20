e365fcac093fd7fd4dd6b05f8972c5ef88f80b1c
Wenting Li <wenting@Wentings-MacBook-Air.local>
2018-04-19 23:11:26 -0400

    organized

--- Deque.java
+++ Deque.java
@@ -1,30 +1,31 @@
 public interface Deque<Pigeon>
 {
+    /*****************COMMENT OUT AND LATHER******************** 
     //Inserts the specified element at the front of this deque unless it 
     //would violate capacity restrictions.
     public boolean offerFirst(Pigeon e);
-
+    
     //Inserts the specified element at the end of this deque unless it 
     //would violate capacity restrictions.
     public boolean offerLast(Pigeon e);
-
+    
     //Retrieves, but does not remove, the first element of this deque, 
     //or returns null if this deque is empty.
     public Pigeon peekFirst();
-
+    
     //Retrieves, but does not remove, the last element of this deque, 
     //or returns null if this deque is empty.
     public Pigeon peekLast();
-
+    
     //Retrieves and removes the first element of this deque, or 
     //returns null if this deque is empty.
     public Pigeon pollFirst();
-
+    
     //Retrieves and removes the last element of this deque, or 
     //returns null if this deque is empty.
     public Pigeon pollLast();
-
+    
     //Returns the number of elements in this deque.
     public int size();
-
+    ******************RINSE AND REPEAT********************/
 }
